a8f0cec8cf1d8cfcb9dbad237be91c57c13de382
saulinkmsg <saulomsg@hotmail.com>
2023-09-19 20:31:08 -0300

    Ola (Console Writeline)

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace BancodeDados
         static void Main(string[] args)
         {
             Console.WriteLine("hello kitty");
+            Console.WriteLine("Ola");
         }
     }
 }
