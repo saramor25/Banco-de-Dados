1f8f3986698cd1d923e1790036d8245a55c0bb17
saulinkmsg <saulomsg@hotmail.com>
2023-09-19 21:24:00 -0300

    Class Item
    Parte 1 - Criação de Voids p/ Inserção, Remoção, Atualização e Pesquisa.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BancodeDados
+[Serializable]
+class Item
 {
-    class Program
+    public int Tag { get; set; }
+    public string Value { get; set; }
+}
+
+class SimpleDatabase
+{
+    private List<Item> items;
+
+    public SimpleDatabase()
     {
-        static void Main(string[] args)
+        items = new List<Item>();
+    }
+
+    public void Insert(int tag, string value)
+    {
+        var newItem = new Item { Tag = tag, Value = value };
+        items.Add(newItem);
+        Console.WriteLine("Inserted: " + newItem.Tag + ", " + newItem.Value);
+    }
+
+    public void Remove(int tag)
+    {
+        var itemToRemove = items.Find(item => item.Tag == tag);
+        if (itemToRemove != null)
         {
-            Console.WriteLine("hello kitty");
-            Console.WriteLine("Ola");
+            items.Remove(itemToRemove);
+            Console.WriteLine("Removed: " + itemToRemove.Tag + ", " + itemToRemove.Value);
+        }
+        else
+        {
+            Console.WriteLine("Item not found");
+        }
+    }
+
+    public void Update(int tag, string newValue)
+    {
+        var itemToUpdate = items.Find(item => item.Tag == tag);
+        if (itemToUpdate != null)
+        {
+            itemToUpdate.Value = newValue;
+            Console.WriteLine("Updated: " + itemToUpdate.Tag + ", " + itemToUpdate.Value);
+        }
+        else
+        {
+            Console.WriteLine("Item not found");
+        }
+    }
+
+    public void Search(int tag)
+    {
+        var itemToSearch = items.Find(item => item.Tag == tag);
+        if (itemToSearch != null)
+        {
+            Console.WriteLine(itemToSearch.Value);
+        }
+        else
+        {
+            Console.WriteLine("Item not found");
         }
     }
 }
