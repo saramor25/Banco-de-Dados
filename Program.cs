ff1ee46ffc1c202d6a74f795aeb19bea350a1f5e
Hdkyuki <hdkyuki8@outlook.com>
2023-09-23 03:12:04 -0300

    Update Program.cs
    part. 2
    
    Implementação dos comandos: inserir, remover e atualizar

--- Program.cs
+++ Program.cs
@@ -65,3 +65,67 @@ class SimpleDatabase
         }
     }
 }
+class Program
+{
+    static void Main(string[] args)
+    {
+        SimpleDatabase database = new SimpleDatabase();
+
+        while (true)
+        {
+            Console.Write("Enter command (insert, remove, update, search, save, load, quit): ");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "quit")
+            {
+                break;
+            }
+
+            string[] tokens = input.Split(' ');
+            string command = tokens[0];
+
+            switch (command)
+            {
+                case "insert":
+                    if (tokens.Length >= 3)
+                    {
+                        int tag = int.Parse(tokens[1]);
+                        string value = string.Join(" ", tokens, 2, tokens.Length - 2);
+                        database.Insert(tag, value);
+                    }
+                    break;
+
+                case "remove":
+                    if (tokens.Length == 2)
+                    {
+                        int tag = int.Parse(tokens[1]);
+                        database.Remove(tag);
+                    }
+                    break;
+
+                case "update":
+                    if (tokens.Length >= 3)
+                    {
+                        int tag = int.Parse(tokens[1]);
+                        string value = string.Join(" ", tokens, 2, tokens.Length - 2);
+                        database.Update(tag, value);
+                    }
+                    break;
+
+                case "search":
+                    if (tokens.Length == 2)
+                    {
+                        int tag = int.Parse(tokens[1]);
+                        database.Search(tag);
+                    }
+                    break;
+
+                
+
+                default:
+                    Console.WriteLine("Invalid command");
+                    break;
+            }
+        }
+    }
+}
