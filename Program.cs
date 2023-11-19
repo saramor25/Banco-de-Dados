4613a70b879decf9a3c08ddcde8bc8647cee98e1
Saikaraii <sarachristine25@gmail.com>
2023-11-19 17:59:31 -0300

    Reajustes e implementação dos RFs
    Ajustando o código de acordo com o feedback da primeira entrega, e ajustando os RF, adicionado comentarios .

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
+// A classe representa um item no banco de dados.
 [Serializable]
 class Item
 {
@@ -11,29 +12,33 @@ class Item
     public string Value { get; set; }
 }
 
+// A classe representa um banco de dados simples.
 class SimpleDatabase
 {
     private List<Item> items;
 
+    // Construtor que inicializa a lista de itens.
     public SimpleDatabase()
     {
         items = new List<Item>();
     }
 
+    // Método para inserir um novo item no banco de dados.
     public void Insert(int tag, string value)
     {
         var newItem = new Item { Tag = tag, Value = value };
         items.Add(newItem);
-        Console.WriteLine("Inserted: " + newItem.Tag + ", " + newItem.Value);
+        Console.WriteLine($"Inserted: {newItem.Tag}, {newItem.Value}");
     }
 
+    // Método para remover um item do banco de dados com base na tag.
     public void Remove(int tag)
     {
         var itemToRemove = items.Find(item => item.Tag == tag);
         if (itemToRemove != null)
         {
             items.Remove(itemToRemove);
-            Console.WriteLine("Removed: " + itemToRemove.Tag + ", " + itemToRemove.Value);
+            Console.WriteLine($"Removed: {itemToRemove.Tag}, {itemToRemove.Value}");
         }
         else
         {
@@ -41,13 +46,14 @@ class SimpleDatabase
         }
     }
 
+    // Método para atualizar o valor de um item no banco de dados com base na tag.
     public void Update(int tag, string newValue)
     {
         var itemToUpdate = items.Find(item => item.Tag == tag);
         if (itemToUpdate != null)
         {
             itemToUpdate.Value = newValue;
-            Console.WriteLine("Updated: " + itemToUpdate.Tag + ", " + itemToUpdate.Value);
+            Console.WriteLine($"Updated: {itemToUpdate.Tag}, {itemToUpdate.Value}");
         }
         else
         {
@@ -55,6 +61,7 @@ class SimpleDatabase
         }
     }
 
+    // Método para buscar um item no banco de dados com base na tag e imprimir o valor.
     public void Search(int tag)
     {
         var itemToSearch = items.Find(item => item.Tag == tag);
@@ -67,6 +74,8 @@ class SimpleDatabase
             Console.WriteLine("Item not found");
         }
     }
+
+    // Método para salvar a lista de itens em um arquivo usando serialização.
     public void SaveToFile(string fileName)
     {
         try
@@ -76,14 +85,19 @@ class SimpleDatabase
                 IFormatter formatter = new BinaryFormatter();
                 formatter.Serialize(fileStream, items);
             }
-            Console.WriteLine("Database saved to file: " + fileName);
+            Console.WriteLine($"Database saved to file: {fileName}");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Error saving database to file: {ex.Message}");
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Error saving database to file: " + ex.Message);
+            Console.WriteLine($"An unexpected error occurred: {ex.Message}");
         }
     }
 
+    // Método para carregar a lista de itens de um arquivo usando desserialização.
     public void LoadFromFile(string fileName)
     {
         try
@@ -93,89 +107,134 @@ class SimpleDatabase
                 IFormatter formatter = new BinaryFormatter();
                 items = (List<Item>)formatter.Deserialize(fileStream);
             }
-            Console.WriteLine("Database loaded from file: " + fileName);
+            Console.WriteLine($"Database loaded from file: {fileName}");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine($"File not found: {fileName}");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Error loading database from file: {ex.Message}");
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Error loading database from file: " + ex.Message);
+            Console.WriteLine($"An unexpected error occurred: {ex.Message}");
         }
     }
 }
+
+// Classe principal que contém o método Main.
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         SimpleDatabase database = new SimpleDatabase();
 
         while (true)
         {
             Console.Write("Enter command (insert, remove, update, search, save, load, quit): ");
-            string input = Console.ReadLine().ToLower();
+            string input = Console.ReadLine()?.ToLower();
 
             if (input == "quit")
             {
                 break;
             }
 
-            string[] tokens = input.Split(' ');
-            string command = tokens[0];
+            string[] tokens = input?.Split(' ');
+            string command = tokens?[0];
 
-            switch (command)
+            try
             {
-                case "insert":
-                    if (tokens.Length >= 3)
-                    {
-                        int tag = int.Parse(tokens[1]);
-                        string value = string.Join(" ", tokens, 2, tokens.Length - 2);
-                        database.Insert(tag, value);
-                    }
-                    break;
+                switch (command)
+                {
+                    case "insert":
+                        if (tokens.Length >= 3)
+                        {
+                            int tag = int.Parse(tokens[1]);
+                            string value = string.Join(" ", tokens, 2, tokens.Length - 2);
+                            database.Insert(tag, value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command. Format: insert <tag> <value>");
+                        }
+                        break;
 
-                case "remove":
-                    if (tokens.Length == 2)
-                    {
-                        int tag = int.Parse(tokens[1]);
-                        database.Remove(tag);
-                    }
-                    break;
+                    case "remove":
+                        if (tokens.Length == 2)
+                        {
+                            int tag = int.Parse(tokens[1]);
+                            database.Remove(tag);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command. Format: remove <tag>");
+                        }
+                        break;
 
-                case "update":
-                    if (tokens.Length >= 3)
-                    {
-                        int tag = int.Parse(tokens[1]);
-                        string value = string.Join(" ", tokens, 2, tokens.Length - 2);
-                        database.Update(tag, value);
-                    }
-                    break;
+                    case "update":
+                        if (tokens.Length >= 3)
+                        {
+                            int tag = int.Parse(tokens[1]);
+                            string value = string.Join(" ", tokens, 2, tokens.Length - 2);
+                            database.Update(tag, value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command. Format: update <tag> <value>");
+                        }
+                        break;
 
-                case "search":
-                    if (tokens.Length == 2)
-                    {
-                        int tag = int.Parse(tokens[1]);
-                        database.Search(tag);
-                    }
-                    break;
+                    case "search":
+                        if (tokens.Length == 2)
+                        {
+                            int tag = int.Parse(tokens[1]);
+                            database.Search(tag);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command. Format: search <tag>");
+                        }
+                        break;
 
-                case "save":
-                    if (tokens.Length == 2)
-                    {
-                        string fileName = tokens[1];
-                        database.SaveToFile(fileName);
-                    }
-                    break;
+                    case "save":
+                        if (tokens.Length == 2)
+                        {
+                            string fileName = tokens[1];
+                            database.SaveToFile(fileName);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command. Format: save <filename>");
+                        }
+                        break;
 
-                case "load":
-                    if (tokens.Length == 2)
-                    {
-                        string fileName = tokens[1];
-                        database.LoadFromFile(fileName);
-                    }
-                    break;
+                    case "load":
+                        if (tokens.Length == 2)
+                        {
+                            string fileName = tokens[1];
+                            database.LoadFromFile(fileName);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid command. Format: load <filename>");
+                        }
+                        break;
 
-                default:
-                    Console.WriteLine("Invalid command");
-                    break;
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
             }
-        }
-    }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format. Please enter a valid number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
 }
